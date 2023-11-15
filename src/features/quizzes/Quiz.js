66f508428f820b75043b42332556f1533dacf710
Michael Hernando <mhernando334@gmail.com>
2023-11-14 19:52:20 -0500

    gh pages fixes

--- src/features/quizzes/Quiz.js
+++ src/features/quizzes/Quiz.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import { Link, useParams, Navigate } from "react-router-dom";
 import Card from "../cards/Card";
--- src/features/topics/Topics.js
+++ src/features/topics/Topics.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import ROUTES from "../../app/routes";
 import { selectTopics } from './topicsSlice.js';
