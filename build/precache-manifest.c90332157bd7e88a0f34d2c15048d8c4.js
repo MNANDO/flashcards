ddbe4023155a898870440449482cee54250bf998
Michael Hernando <mhernando334@gmail.com>
2023-11-15 19:37:51 -0500

    c

---
+++ build/precache-manifest.c90332157bd7e88a0f34d2c15048d8c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b73370d7d7e32228b363c13d417e979e",
+    "url": "/flashcards/index.html"
+  },
+  {
+    "revision": "cbb188dc738bbe80a917",
+    "url": "/flashcards/static/css/main.563e4f40.chunk.css"
+  },
+  {
+    "revision": "87f39a3aaabce5a4c5c9",
+    "url": "/flashcards/static/js/2.4ca1a406.chunk.js"
+  },
+  {
+    "revision": "cebd7cb213b599a49806c5f4125076a3",
+    "url": "/flashcards/static/js/2.4ca1a406.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ad00fcf5a43afb28206",
+    "url": "/flashcards/static/js/3.1de67089.chunk.js"
+  },
+  {
+    "revision": "cbb188dc738bbe80a917",
+    "url": "/flashcards/static/js/main.40d8d827.chunk.js"
+  },
+  {
+    "revision": "f2b9ef334f73aebf3de3",
+    "url": "/flashcards/static/js/runtime-main.bb28274d.js"
+  }
+]);
